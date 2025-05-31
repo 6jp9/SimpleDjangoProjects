0c33609032ad12b2356f6152d7c8ae4338c31a8a
6jp9 <jayprakash1405401@gmail.com>
2025-05-31 22:54:39 +0530

    Test Project

--- Movie_Project/movieapp/views.py
+++ Movie_Project/movieapp/views.py
@@ -13,16 +13,28 @@ def index(request):
 def detail(request,movie_id):
     movie=Movie.objects.get(id=movie_id)
     return render(request,"details.html",{'movie':movie})
+from django.http import HttpResponse
+
+
 
 def add_movie(request):
-    if request.method=="POST":
-        name=request.POST.get('name',)
-        desc = request.POST.get('desc',)
-        year = request.POST.get('year',)
-        img = request.FILES['img']
-        movie=Movie(name=name,desc=desc,year=year,img=img)
-        movie.save()
-    return render(request,'add.html')
+    if request.method == "POST":
+        name = request.POST.get('name', None)
+        desc = request.POST.get('desc', None)
+        year = request.POST.get('year', None)
+        img = request.FILES.get('img', None)
+
+        if name and desc and year and img:
+            movie = Movie(name=name, desc=desc, year=year, img=img)
+            movie.save()
+            return render(request, 'add.html')  
+        return HttpResponse(
+            "<center><h2>Please enter all the fields correctly!!</h2><br><a href='/add' class='btn btn-primary' >cancel & go back?</a></center>"
+        )
+
+    return render(request, 'add.html')
+
+
 
 def update(request,id):
     movie=Movie.objects.get(id=id)
