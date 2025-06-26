17444b6b475b97256dd634b227dd770803654d5b
6jp9 <jayprakash1405401@gmail.com>
2025-06-26 11:56:53 +0530

    90% done

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -76,13 +76,20 @@ LOGOUT_REDIRECT_URL = '/logout'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'testdb',
+#         'USER':'root',
+#         'PASSWORD':'1234',
+
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testdb',
-        'USER':'root',
-        'PASSWORD':'1234',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
